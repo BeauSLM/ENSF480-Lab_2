f1d8c1659fa0faea791c419e5d0cb378b385b9b5
BeauSLM <mccartney.beausl@gmail.com>
2021-09-27 14:25:34 -0600

    Refactor Point.h
    Changes:
    
    - Add getters/setters - they're required
    
    - Add conditional compilation - good practice
    
    - Add shape as a friend class - shape's manage their own points, nothing
        else has the ability to

--- exB/Point.h
+++ exB/Point.h
@@ -1,15 +1,21 @@
 // TODO: Documentation!
+// TODO: " Access functions, getters and setters, as needed. " -- maybe???
+#ifndef POINT_H
+#define POINT_H
 class Point {
     private:
         double x, y;
         static int id;
+        friend class Shape;
     public:
         Point(double x, double y):
-            x(x), y(y) {}
-        void display() const;
+            x(x), y(y) {} // TODO: initialize id (how?)
+        double getX() const { return x; }
+        double getY() const { return y; }
 
-        // static?
-        static int counter() { return id - 1000; }
-        static double distance(const Point&, const Point&);
-        double distanceTo(const Point&) const;
+        void display() const;
+        static int counter() { return id - 1000; } // static?
+        static double distance(const Point& the_point, const Point& other);
+        double distance(const Point& other) const;
 };
+#endif
