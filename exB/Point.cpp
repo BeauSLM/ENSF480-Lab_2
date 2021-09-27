5172dd3c18270886c4dfb4fc54fa45f89835f914
BeauSLM <mccartney.beausl@gmail.com>
2021-09-27 14:26:54 -0600

    Implement class Point

---
+++ exB/Point.cpp
@@ -0,0 +1,25 @@
+// TODO: i think we may need to make a separate function
+// to print doubles in the format #####.## as specified
+// in the assignment
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include "Point.h"
+using namespace std;
+
+void Point::display() const {
+    cout << fixed << "X-coordinate: " << setw(8) << setprecision(2) << x << endl;
+    cout << fixed << "Y-coordinate: " << setw(8) << setprecision(2) << y << endl;
+}
+
+double Point::distance(const Point& the_point, const Point& other) {
+    double dx = the_point.getX() - other.getX();
+    double dy = the_point.getY() - other.getY();
+
+    return sqrt(dx * dx + dy * dy);
+}
+
+double Point::distance(const Point& other) const {
+    return distance(*this, other);
+}
